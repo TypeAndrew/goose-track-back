ceb3c85ad32014f8cc946f1dff0c061dc26d72e1
shulyar <andy.shular@gmail.com>
2023-03-09 00:41:16 +0200

    node rest api

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,59 @@
 const express = require('express')
-
+const uuid = require('uuid').v4;
 const router = express.Router()
 
+const contacts = require('../../models/contacts')
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  
+  const data = await contacts.listContacts();
+  console.log(data);
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      data,
+    },
+  });
+
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params;
+    const data = await contacts.listContacts();
+    console.log(req);
+    const foundElement = await data.filter(el => el.id === contactId);
+    if (  foundElement.length === 0) {
+        console.log(`Element with ${contactId}  not found`.magenta);
+    } 
+     
+    
+    res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      foundElement,
+    },
+  });
+       
+
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { name, email, phone } = req.query;
+  console.log(req.query)
+  const data = (await contacts.listContacts() === undefined) ? [] : await contacts.listContacts();
+  const newdata = [...data, { id: uuid.apply(), name: name, email: email, phone: phone }];
+  await contacts.saveFile( newdata);
+  console.log(`Element with ${name} ${email} ${phone}id was added`.green);
+   
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      newdata,
+    },
+  });
 })
 
 router.delete('/:contactId', async (req, res, next) => {
