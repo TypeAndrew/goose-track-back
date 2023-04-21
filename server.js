bd73ee694d7b137d5a49d1d8668769ceafc27b8f
shulyar <andy.shular@gmail.com>
2023-04-21 17:43:36 +0300

    START PATTERN

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('./app')
+const port = process.env.PORT;
 
-app.listen(3000, () => {
-    console.log("Server running. Use our API on port: 3000")
-})
+app.listen(port, () => {
+    console.log(`Server running. Use our API on port: ${port}`)
+});
