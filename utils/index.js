5180584e48dde01a676fdee68933752039591a00
shulyar <andy.shular@gmail.com>
2023-04-22 07:30:44 +0300

    START PATTERN

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,9 @@
-// const AppError = require('./appError');
+const AppError = require('./appError');
 const catchAsync = require('./catchAsync');
 const validators = require('./joyValidators');
 
 module.exports = {
- // AppError,
+  AppError,
   catchAsync,
   validators,
 }; 
